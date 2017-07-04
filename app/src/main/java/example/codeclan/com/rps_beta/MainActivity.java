e42786e51c30ec85447015a2b1f5c4647296b137
David Clarkson <clarkee013@gmail.com>
2017-07-04 20:57:50 +0100

    MainActivity started, new game, new computer, new player entered, main activity page with layout completed, button press to player move still to do, computer moves still to do, compare moves still to do, display winner & second activity (results) still to do.

--- app/src/main/java/example/codeclan/com/rps_beta/MainActivity.java
+++ app/src/main/java/example/codeclan/com/rps_beta/MainActivity.java
@@ -2,19 +2,45 @@ package example.codeclan.com.rps_beta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Game game;
+    Player player;
+    Computer computer;
+    Button rockButton;
+    Button paperButton;
+    Button scissorButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        game = new Game();
+        player = new Player();
+        computer = new Computer();
+
+        rockButton = (Button)findViewById(R.id.rockButton);
+        paperButton = (Button)findViewById(R.id.paperButton);
+        scissorButton = (Button)findViewById(R.id.scissorsButton);
+
     }
 
-    //TODO new up a game
-    //TODO new CPU
-    //TODO new Player
-    //TODO layout buttons an shit
+    public void onButtonClickSetPlayerMove(){
+
+    }
+
+
+
+
+
+
+    //TODO new up a game - done
+    //TODO new CPU - done
+    //TODO new Player - done
+    //TODO layout buttons an shit - done
     //TODO when button is pressed it sets the players move
     //TODO computer moves
     //TODO compare moves
